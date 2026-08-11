6c84514fbb44c00d9baf2ff8f0528c965d5e5875
Evan Raskob <evan@flkr.com>
2026-08-11 11:54:22 +0100

    cleanup pages

--- src/js/constants/priority.js
+++
@@ -1,9 +0,0 @@
-
-    // priority of sent commands in async queue
-    exports.highestPriority = 2;
-    exports.highPriority = 3;
-    exports.normalPriority = 4;
-    exports.lowPriority = 5;
-    exports.lowestPriority = 6;
-    
-
